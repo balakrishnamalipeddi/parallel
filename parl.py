4daeadbbdbfec90a948ab372e6b97197590172a6
balakrishnamalipeddi <165142986+balakrishnamalipeddi@users.noreply.github.com>
2024-04-12 10:57:40 +0530

    Update parl.py

--- parl.py
+++ parl.py
@@ -1,28 +1,40 @@
 pipeline {
-    agent any
-    
+    agent none
+
     stages {
-        stage('Parallel Stage') {
-            parallel {
-                stage('Stage A') {
-                    steps {
-                        echo 'Executing Stage A'
-                        
-                    }
+        stage('Build and run') {
+          parallel {
+            stage('master-agent-pipeline') {
+              agent {label 'master'}
+              stages{
+                stage('Build') {
+                steps {
+                  echo 'Building..'
+                  }
                 }
-                stage('Stage B') {
-                    steps {
-                        echo 'Executing Stage B'
-                        
-                    }
+                stage('Test') {
+                  steps {
+                    echo 'Testing..'
+                 }
                 }
-                stage('Stage C') {
-                    steps {
-                        echo 'Executing Stage C'
-                        
-                    }
+	       }
+              }
+            stage('ubuntu-agent-pipeline') {
+              agent {label 'ubuntu'}
+              stages{
+                stage('Build') {
+                steps {
+                  echo 'Building..'
+                  }
                 }
+                stage('Test') {
+                  steps {
+                    echo 'Testing..'
+                 }
+                }
+               }
+              }
+             }
             }
-        }
-    }
-}
+           }
+          }
